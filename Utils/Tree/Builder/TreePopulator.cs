624bf02a9d70d9631dfb1313caf3ab325bcdc421
ncguy2 <nick.guy@hotmail.co.uk>
2016-08-17 17:14:04 +0100

    Small change to the TreePopulator
    Now provides a default method of generating the nodes

--- Utils/Tree/Builder/TreePopulator.cs
+++ Utils/Tree/Builder/TreePopulator.cs
@@ -4,16 +4,24 @@ using System.Collections.Generic;
 namespace Utils.Tree.Builder {
     public class TreePopulator {
 
-        public static void Populate<T, U>(VisitableTree<T> tree, List<U> data, char delim, Func<U, String> stringLoc,
-            Func<U, String, T> nodeLoc) {
+        public static void Populate<U>(VisitableTree<TreeObjectWrapper<U>> tree, List<U> data, char delim, Func<U, String> stringLoc) {
+            Func<U, String, TreeObjectWrapper<U>> nodeLoc = (u, s) => {
+                if(s.Equals(stringLoc.Invoke(u))) return new TreeObjectWrapper<U>(u, s);
+                return new TreeObjectWrapper<U>(s);
+            };
+            Populate(tree, data, delim, stringLoc, nodeLoc);
+        }
+
+        public static void Populate<U>(VisitableTree<TreeObjectWrapper<U>> tree, List<U> data, char delim, Func<U, String> stringLoc,
+            Func<U, String, TreeObjectWrapper<U>> nodeLoc) {
             Populate(tree, data, delim, stringLoc, nodeLoc, false);
         }
 
-        public static void Populate<T, U>(VisitableTree<T> tree, List<U> data, char delim, Func<U, String> stringLoc,
-            Func<U, String, T> nodeLoc, bool fullPath) {
-            VisitableTree<T> current = tree;
+        public static void Populate<U>(VisitableTree<TreeObjectWrapper<U>> tree, List<U> data, char delim, Func<U, String> stringLoc,
+            Func<U, String, TreeObjectWrapper<U>> nodeLoc, bool fullPath) {
+            VisitableTree<TreeObjectWrapper<U>> current = tree;
             foreach (U u in data) {
-                VisitableTree<T> root = current;
+                VisitableTree<TreeObjectWrapper<U>> root = current;
                 String pathStr = stringLoc.Invoke(u);
                 String[] path = pathStr.Split(delim);
                 String p = "";
